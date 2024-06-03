0fb819dea6552a5d83f0c2a2133dca92580009f8
Mist Uber <106929734+micio-san@users.noreply.github.com>
2024-06-03 15:16:08 +0200

    Update Pong.cpp
    D:

--- Pong.cpp
+++ Pong.cpp
@@ -3,69 +3,140 @@
 
 #include <iostream>
 #include <raylib.h>
+#include <string>
 using namespace std;
 
-const int width = 1280;
-const int height = 800;
+const int screenWidth = 1280;
+const int screenHeight = 800;
+
+int playerScore = 0;
+int cpuScore = 0;
 
 class Ball {
+
 public:
-    float X, Y;
-    int Speed_x, Speed_y;
-    int Radius;
-
-    void drawBall() {
-        //coordinates x y string
-        DrawCircle(X, Y, Radius, PINK);
-    }
-
-    void changeCoordinates() {
-        X += Speed_x;
-        Y += Speed_y;
-        if ((Y + Radius) >= height || (Y + Radius) <= 0) {
-            Speed_y *= -1;
-        }
-        if ((X + Radius) >= width || (X + Radius) <= 0) {
-            Speed_x *= -1;
-        }
-    }
-
+	float ballRadius,
+	      y,
+	      x;
+	int ballSpeedX,
+	    ballSpeedY;
+	Ball(int radius,float posX,float posY, int speedX,int speedY) {
+		this->ballRadius = radius;
+		this->y = posY;
+		this->x = posX;
+		this->ballSpeedX = speedX;
+		this->ballSpeedY = speedY;
+	}
+	void drawBall() {
+		DrawCircle(x,y,ballRadius,PURPLE);
+	}
+	void UpdateBall() {
+		x += ballSpeedX;
+		if ((x + 60) > screenWidth) {
+			playerScore++;
+			ballSpeedX *= -1;
+			ballSpeedY *= -1;
+		} 
+		if ((x - 60) <= 0) {
+			/*ballSpeedX *= -1;*/
+			cpuScore++;
+			ballSpeedX *= -1;
+			ballSpeedY *= -1;
+		}
+		y += ballSpeedY;
+		if (((y + 80) > screenHeight) || (y-80)<=0) {
+			ballSpeedY *= -1;
+		}
+	}
 };
 
-Ball ball;
+class Paddle {
+protected: 
+	void LimitMovement() {			
+		if ((y + 220)> screenHeight) {
+				y = (screenHeight - 220);
+			}
+			if (y <= 20) {
+				y = 20;
+			}
+	};
+public:
+	float 
+		  height = 200,
+	      width = 60,
+	      x,
+	      y;
+	int paddleSpeedY = 6;
+	Paddle(float posY, int posX) {
+		this->y = posY;
+		this->x = posX;
+	}
+	void DrawPaddle() {
+		DrawRectangle(x, y, width, height, PURPLE);
+	}
+	//sets screen limits
+
+	void UpdatePaddle() {
+		if (IsKeyDown(KEY_DOWN) == true) {
+			y += paddleSpeedY;
+
+		}
+		if (IsKeyDown(KEY_UP) == true) {
+			y -= paddleSpeedY;
+		}
+		LimitMovement();
+
+	}
+};
+
+class PCPaddle :public Paddle {
+public:
+	PCPaddle(float posY, int posX) :Paddle(posY, posX) {}
+	void UpdatePaddle(int ball_y) {
+		if (y < ball_y) {
+			y += paddleSpeedY;
+		}
+		else {
+			y -= paddleSpeedY;
+		} 
+		LimitMovement();
+   }
+};
 
 int main()
 {
-    //initializes a window with 3 arguments, wh of window
-    InitWindow(width, height, "Pong");
-    //game loop
-    /*Decide how fast the game should run by picking fps, if not decided the pc will try to run it as
-    fast as possible */
-    SetTargetFPS(60);
-    int paddleY = (height / 2) - 80;
-    ball.X = width / 2;
-    ball.Y = height / 2;
-    ball.Radius = 40;
-    ball.Speed_x = 7;
-    ball.Speed_y = 7;
+	InitWindow(screenWidth, screenHeight, "My pong game");
+	SetTargetFPS(60);
+	Ball ball = Ball(30, (screenWidth/2), (screenHeight / 2), 6, 6);
+	Paddle myPaddle = Paddle(((screenHeight / 2) - 100), 40);
+	PCPaddle pcPaddle = PCPaddle(((screenHeight / 2) - 100), (screenWidth -80));
+	while (WindowShouldClose()==false) {
+		BeginDrawing();
+		ball.drawBall();
+		myPaddle.DrawPaddle();
+		pcPaddle.DrawPaddle();
+		//checking 4 collisions
+           if (CheckCollisionCircleRec(Vector2{ ball.x, ball.y }, ball.ballRadius, Rectangle{ myPaddle.x, myPaddle.y,
+																						  myPaddle.width, myPaddle.height }) == true) {
+			ball.ballSpeedX *= -1;
+			ball.ballSpeedY *= -1;
 
-    //Window should close checks if the escape key or the close icon is pressed, return true if that happens
-    while (WindowShouldClose() == false) {
-        //Initialize a blank canvas
-        BeginDrawing();
-        ball.changeCoordinates();
-        //resetting the background before drawing again
-        ClearBackground(BLACK);
-        //separate the two sides, start x start y end x end y color
-        DrawLine((width/2),0,( width / 2), 800, WHITE);
-        ball.drawBall();
-        //coordinates x y width height color
-        DrawRectangle(20, paddleY, 60, 180, PINK);
-        DrawRectangle(1200, (height/2)-80, 60, 180, PINK);
-        EndDrawing();
-    }
-
-    //every instance of window has to be destroyed
-    CloseWindow();
-    return 0;
+		   };
+		   if (CheckCollisionCircleRec(Vector2{ ball.x, ball.y }, ball.ballRadius, Rectangle{ pcPaddle.x, pcPaddle.y,
+																				  pcPaddle.width, pcPaddle.height }) == true) {
+			ball.ballSpeedX *= -1;
+			ball.ballSpeedY *= -1;
+		    }
+		ClearBackground(BLACK);
+		DrawLine((screenWidth/2),0, (screenWidth / 2),screenHeight, PURPLE);
+		ball.UpdateBall();
+		myPaddle.UpdatePaddle();
+		pcPaddle.UpdatePaddle(ball.y);
+		
+		DrawText(TextFormat("%d", playerScore),0,0,22,PINK);
+		EndDrawing();
+	}
+	 
+	CloseWindow();
+	return 0;
 }
